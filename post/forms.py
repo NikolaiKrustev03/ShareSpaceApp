558ea126aad5fb788936a9aea6afb59e8ad9e8df
NikolaiKrustev03 <n.krastev2003@gmail.com>
2024-12-13 18:51:54 +0200

    Added missing add tag feature

--- post/forms.py
+++ post/forms.py
@@ -5,12 +5,13 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'image1', 'image2', 'image3']
+        fields = ['title', 'content', 'tag', 'image1', 'image2', 'image3']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['title'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter the post title'})
         self.fields['content'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter the post content'})
+        self.fields['tag'].widget.attrs.update({'class': 'form-control'})
         self.fields['image1'].widget.attrs.update({'class': 'form-control-file'})
         self.fields['image2'].widget.attrs.update({'class': 'form-control-file'})
         self.fields['image3'].widget.attrs.update({'class': 'form-control-file'})
